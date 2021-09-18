eaacaa310769da02587c61e64b33506f91121f34
vv_tomilin <backatom1@gmail.com>
2021-09-18 11:46:32 +0500

    Повторение контекста THIS

---
+++ TRAINING/prototype_understanding/this.js
@@ -0,0 +1,69 @@
+//* Повторение THIS
+
+function fn() {
+    console.log(this);
+    console.log(`"this" is window - ${this === window}`);
+    console.log(`"this" is obj - ${this === obj}`);
+}
+
+const obj = {};
+
+fn();
+
+obj.fn = fn;
+
+obj.fn();
+
+//* ------------------------------------------------------------------//
+window.a = 1;
+
+const printA = () => {
+    console.log(this.a);
+    console.log(`"this" is window to arrow func - ${this === window}`);
+};
+
+printA();
+
+const obj2 = {
+    a: 12,
+};
+
+obj2.printA = printA;
+obj2.printA(); //* выведет также "1", а не "5" 
+//*            //  так как "printA" стрелочная функция
+//*            //  и не имеет своего контекста вызова
+
+//* ---------------------------------------------------------------------//
+
+const users = [];
+
+function getFullName () {
+	return `${this.name} ${this.surname}`
+}
+
+function callBy (array, method) {
+	array.map((user) => {
+    user.fullName = method.bind(user);
+    console.log(user.fullName());
+    console.log(user.fullName === this.getFullName);
+    console.log(this.getFullName);
+    console.log(user);
+  });
+}
+
+users.push({
+	name: "Алексей",
+	surname: "Данчин",
+})
+
+users.push({
+	name: "Тимофей",
+	surname: "Данчин",
+})
+
+users.push({
+	name: "Ольга",
+	surname: "Марко",
+})
+
+callBy(users, getFullName);

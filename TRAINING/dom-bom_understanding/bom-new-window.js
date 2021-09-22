e625f48b8de2d795abf9a7d98622fe4b330b00fe
vv_tomilin <backatom1@gmail.com>
2021-09-22 16:32:21 +0500

    НЕбольшая практика с BOM

---
+++ TRAINING/dom-bom_understanding/bom-new-window.js
@@ -0,0 +1,15 @@
+const targetOrigin = 'file:///C:/Users/dnsuser/Desktop/JS%20Learning/JavaScript/Mini_tasks/TRAINING/dom-bom_understanding/bom-new-window.html';
+
+// window.addEventListener('message', function(e) {
+//     if(e.origin !== targetOrigin) {
+//         return console.log('Не тот домен');
+//     }
+
+//     alert(`message: ${e.data}`);
+// });
+
+console.log(
+    window.addEventListener('message', function(e) {
+        return e.data;
+    })
+);
---
+++ TRAINING/dom-bom_understanding/bom.js
@@ -0,0 +1,19 @@
+
+const btnOpenNewTab = document.querySelector('#open-new-tab');
+const btnOpenNewWindow =document.querySelector('#open-new-window');
+
+btnOpenNewTab.addEventListener('click', () => {
+    window.open();
+});
+
+btnOpenNewWindow.addEventListener('click', () => {
+    const url = 'file:///C:/Users/dnsuser/Desktop/JS%20Learning/JavaScript/Mini_tasks/TRAINING/dom-bom_understanding/bom-new-window.html';
+    const data = {
+        name: 'Vasily',
+        surename: 'Tomilin',
+        age: 30
+    };
+    
+    const newWindow = window.open(url, 'New Window', 'width=500, height=500, top=150, left=150');
+    newWindow.postMessage(data, url);
+});

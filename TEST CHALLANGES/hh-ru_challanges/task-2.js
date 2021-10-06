ca1b63ea2b5854f87f36a046d4e42c8ab9d36243
vv_tomilin <backatom1@gmail.com>
2021-10-06 21:02:43 +0500

    Смог правильно настроить счетчик стека чисел, также смог правильно настроить работу с концом строки. Переделал нсколько раз логику счетчика

--- TEST CHALLANGES/hh-ru_challanges/task-2.js
+++ TEST CHALLANGES/hh-ru_challanges/task-2.js
@@ -1,43 +1,91 @@
-const str1 = 'd6+d2+(d2+20)+25';
-console.log(str1);
+const str1 = '6+d2+(d2855+d2*3+30)+4548+d48';
+const str2 = '2*133';
+console.log(str2);
 
 const numEtalon = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 const operators = ['*', '+', '-', '>', '(', ')'];
 
+// const operatorsPriority = {
+//     greater: 1,        //? ">"
+//     addition: 2,       //? "+"
+//     subtraction: 2,    //? "-"
+//     multiplication: 3, //? "*"
+// };
+
 let numStack = [];
 let operatorsStack = [];
 
-parserStr(str1);
+parserStr(str2);
 
 function parserStr(str) {
 
     const strElements = str.split('');
 
+    let numStackCounter = 0; //* счетчик числового стека
+
     for (let i = 0; i < strElements.length; i++) {
 
-        const isNumberPrevChar = numEtalon.includes(strElements[i - 1]);
+        const isNumberPrevChar = numEtalon.includes(strElements[i - 1]); //* является ли числом предыдущий символ
 
+        const numStackTopCurrent = numStack[numStack.length - 1]; //* верхний элемент стэка
+
+        //* Формирую токен из числа или dN и ложу его в стэк с числами
         if (strElements[i] === 'd') {
             numStack.push(strElements[i]);
+
+            numStackCounter +=1;
         }
 
         if (isNumber(strElements[i])) {
             const numToken = [];
 
+            //* двузначный токен
             if (isNumberPrevChar || strElements[i - 1] === 'd') {
-                createTokenForNumStack(numToken, numStack, strElements[i - 1], strElements[i]);
+                numToken.push(strElements[i - 1]);
+                numToken.push(strElements[i]);
+
+                numStack.pop(numStackTopCurrent);
+                numStackCounter -= 1;
+
+                numStack.push(numToken.join(''));
+                numStackCounter +=1;
             } else {
                 numStack.push(strElements[i]);
+                numStackCounter +=1;
+            }
+
+            //* токен с количеством символов > 2
+            if (numStack.length > 1) {
+
+                if (numStackTopCurrent.split('').length > 1 && isOperator(strElements[i - numStackTopCurrent.split('').length - 1])) {
+                    const bigNumToken = [...numStackTopCurrent.split('')];
+
+                    bigNumToken.push(strElements[i]);
+
+                    numStack.pop(numStackTopCurrent);
+                    numStackCounter -= 1;
+
+                    numStack.push(bigNumToken.join(''));
+                    numStackCounter +=1;
+                }
             }
         }
 
+        //* формирую токен из оператора и ложу его в стэк с операторами
         if (isOperator(strElements[i])) {
             operatorsStack.push(strElements[i]);
         }
+
+        if (i + 1 > strElements.length - 1) {
+            console.log('End', numStackCounter, numStack[numStackCounter - 1]);
+        }
     }
+
+    console.log('Counter: ',numStackCounter);
 
     console.log("Numbers: ", numStack);
     console.log("Operators: ", operatorsStack);
+    
 }
 
 function isNumber(char) {
@@ -46,13 +94,5 @@ function isNumber(char) {
 
 function isOperator(char) {
     return operators.includes(char);
-}
-
-function createTokenForNumStack(token, stack, prevChar, currentChar) {
-    token.push(prevChar);
-    token.push(currentChar);
-
-    stack.pop(prevChar);
-    stack.push(token.join(''));
 }
 

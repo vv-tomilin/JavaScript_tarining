c3fec4392f8bc2d9e65934763389c7f951a10da8
vv_tomilin <backatom1@gmail.com>
2021-10-08 11:25:30 +0500

    Настроил и протестировал правильное формирование числовых токенов и записи их в стек с числами. Багов на данный момент не обнаружил.

--- TEST CHALLANGES/hh-ru_challanges/task-2(v2).js
+++ TEST CHALLANGES/hh-ru_challanges/task-2(v2).js
@@ -4,16 +4,15 @@ const str1 = '2+1';
 const str2 = '21+23+45';
 const str3 = 'd2+d45+3+d789';
 const str4 = 'd665+68+d155';
+const str5 = 'd5646354654+651616464646+d48484+d5566';
 
-console.log(str4);
+const currentStr = str3;
 
 const numsEtalon = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 const opersEtalon = ['*', '+', '-', '>', '(', ')'];
 
 let numStack = [];
 let operatorsStack = [];
-
-calcVariations(str4);
 
 function calcVariations(str) {
     
@@ -32,16 +31,21 @@ function calcVariations(str) {
 
         const endLine = i + 1 > charsArr.length - 1;
 
+        const topStackElem = numStack[numStackCounter - 1];
+
         if (isNumberCurr || currentChar === 'd') {
             tokenCreator.push(currentChar);
         } 
         else if (isOperatorCurr) {
+
             const token = tokenCreator.join('');
 
             numStack.push(token);
             numStackCounter += 1;
 
             tokenCreator = [];
+
+            console.log('Top elem OPER =>', numStack[numStackCounter - 1], numStack, 'Count', numStackCounter); //! ==================
         }
 
         if (endLine) {
@@ -52,7 +56,12 @@ function calcVariations(str) {
 
             tokenCreator = [];
         }
+
+        console.log('Top elem END =>', numStack[numStackCounter - 1], numStack, 'Count', numStackCounter); //! ==================
     }
 
     console.log(numStack, numStackCounter);
 }
+
+console.log(currentStr);
+calcVariations(currentStr);

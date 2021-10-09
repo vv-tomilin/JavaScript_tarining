13c45620b7752d241932ff90e9f2ccb469e8ca30
vv_tomilin <backatom1@gmail.com>
2021-10-09 19:23:29 +0500

    Исправленна ошибка обозначенная в предыдущем коммите. Проблема заключалась в том, что в стеке Синтаксического анализатора после выхода из основного цикла оставалось 2 оператора, один из которых не выталкивался в выходной массив

--- TEST CHALLANGES/hh-ru_challanges/Task-2, Polish Notation/pol_not.js
+++ TEST CHALLANGES/hh-ru_challanges/Task-2, Polish Notation/pol_not.js
@@ -1,15 +1,16 @@
 'use strict';
 
 const str1 = '2+1';
-const str2 = '(1*21+23)*45'; //! WARNING - с таким выражением (если в "()" > 2 операндов и 1 оператора) выходит неправильная нотация
-const str3 = 'd2+d45+3+d789';
+const str2 = '(2+21*23)+45'; 
+const str3 = 'd2+d45+(3+d789)';
 const str4 = 'd665+68+d155';
 const str5 = 'd5646354654+651616464646+d48484+d5566';
 const str6 = '21+25+45+5+2';
-const str7 = '(5>4)*(2>1)+3';
+const str7 = '(1>4)*(4>3)+3';
 const str8 = '10+5*2-1*3*6';
+const str9 = '(1>3)+(3>2)*2';//! WARNING почему то в стеке остается 2 числа вместо 1 и результат неверен (ошибка где то в синтаксическом анализаторе)
 
-const currentStr = str2;
+const currentStr = str9;
 
 const numsEtalon = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'd'];
 const opersEtalon = ['*', '+', '-', '>', '(', ')'];
@@ -17,8 +18,88 @@ const opersEtalon = ['*', '+', '-', '>', '(', ')'];
 const calcArr = tokensArrCreated(currentStr);
 console.log(currentStr);
 console.log(calcArr);
-
 console.log('Exit RPN:', createRPN (calcArr));
+
+const rpnArr = createRPN (calcArr);
+
+calculateVariations(rpnArr);
+
+function calculateVariations(rpnArr) {
+
+    let stack = [];
+    let resEasy = 0;
+
+    for (let i = 0; i < rpnArr.length; i++) {
+
+        const currentToken = rpnArr[i];
+
+        const isNumber = (str) => {
+            const chars = str.split('');
+            return numsEtalon.includes(chars[0]);
+        };
+        const isOperator = opersEtalon.includes(currentToken);
+
+        if (isNumber(currentToken) || currentToken === 'd') {
+
+            stack.push(currentToken);
+        }
+
+        if (isOperator) {
+
+            if (!stack[stack.length - 1].includes('d') && !stack[stack.length - 2].includes('d')) {
+
+                switch (currentToken) {
+                    case '*':
+                        const calc1 = Number.parseInt(stack[stack.length - 2]) * Number.parseInt(stack[stack.length - 1]);
+
+                        stack.pop(stack[stack.length - 1]);
+                        stack.pop(stack[stack.length - 2]);
+
+                        stack.push(String(calc1));
+                        break;
+
+                    case '+':
+                        const calc2 = Number.parseInt(stack[stack.length - 2]) + Number.parseInt(stack[stack.length - 1]);
+
+                        stack.pop(stack[stack.length - 1]);
+                        stack.pop(stack[stack.length - 2]);
+
+                        stack.push(String(calc2));
+                        break;
+                    case '-':
+                        const calc3 = Number.parseInt(stack[stack.length - 2]) - Number.parseInt(stack[stack.length - 1]);
+
+                        stack.pop(stack[stack.length - 1]);
+                        stack.pop(stack[stack.length - 2]);
+
+                        stack.push(String(calc3));
+                        break;
+                    case '>':
+                        const log = Number.parseInt(stack[stack.length - 2]) > Number.parseInt(stack[stack.length - 1]);
+
+                        let calc4 = '';
+
+                        if (log) {
+                            calc4 = '1';
+                        } else {
+                            calc4 = '0';
+                        }
+
+                        stack.pop(stack[stack.length - 1]);
+                        stack.pop(stack[stack.length - 2]);
+
+                        stack.push(calc4);
+                        break;
+                }
+            }
+        }
+    }
+
+    resEasy = stack[stack.length - 1];
+
+    console.log('Результат', resEasy);//! 000000000000000000000000
+    console.log(stack); //! 000000000000000000000000
+}
 
 
 function createRPN (calcArr) {
@@ -158,10 +239,14 @@ function createRPN (calcArr) {
         }
     }
 
-    exitArr.push(stack[0]);
-    stack.pop(stack[0]);
+    while (stack.length > 1) {
+        exitArr.push(stack[stack.length - 1]);
+        stack.pop(stack[stack.length - 1]);
+    }
 
-    console.log(stack);
+    exitArr.push(stack[stack.length - 1]);
+    stack.pop(stack[stack.length - 1]);
+
     return exitArr;
 }
 

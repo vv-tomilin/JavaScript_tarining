6ecb9b16ad8d3cf42fb826350e917feb655b6b6f
vv_tomilin <backatom1@gmail.com>
2021-10-09 10:39:42 +0500

    Настроил код так, что сейчас вычисления правильны и с операторами разного приоритета, но пока без скобок

--- TEST CHALLANGES/hh-ru_challanges/task-2(v3).js
+++ TEST CHALLANGES/hh-ru_challanges/task-2(v3).js
@@ -7,9 +7,9 @@ const str4 = 'd665+68+d155';
 const str5 = 'd5646354654+651616464646+d48484+d5566';
 const str6 = '21+25-45+5+2';
 const str7 = '(5>4)*2';
-const str8 = '10+5*2';
+const str8 = '10+5*2-1*3*6'; // 
 
-const currentStr = str6;
+const currentStr = str8;
 
 const numsEtalon = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'd'];
 const opersEtalon = ['*', '+', '-', '>', '(', ')'];
@@ -26,6 +26,12 @@ function calcVariations(tokensArr=[]) {
     let result = 0;
 
     for (let i = 0; i < tokensArr.length; i++) {
+
+        console.log('START i',
+            i , 'Num st=>', numStack, 'Oper st=>', operStack, 
+            'Top num', numStack[numStack.length - 1], 
+            'Top op', operStack[operStack.length - 1],
+            'Cur tok:', tokensArr[i]); //! START 000000000000000000000000000000000000000000
         
         const currentToken = tokensArr[i];
 
@@ -41,11 +47,12 @@ function calcVariations(tokensArr=[]) {
         }
         
         if (isOperator) {
+
             if (operStack.length === 0) {
 
                 operStack.push(currentToken);
 
-            } else if (numStack.length > 1) {
+            } else {
 
                 const topOper = operStack[operStack.length - 1];
                 const topNum = numStack[numStack.length - 1];
@@ -251,9 +258,105 @@ function calcVariations(tokensArr=[]) {
                     break;
             }
         }
+
+        if (i + 1 > tokensArr.length - 1 && operStack.length > 1) {
+
+            while (operStack.length > 1) {
+
+                const topOper = operStack[operStack.length - 1];
+                const topNum = numStack[numStack.length - 1];
+                const prevNum = numStack[numStack.length - 2];
+
+                switch (topOper) {
+                    case '*':
+                        if (!topNum.includes('d') && !prevNum.includes('d')) {
+                            const calc = Number.parseInt(prevNum) * Number.parseInt(topNum);
+
+                            numStack.pop(topNum);
+                            numStack.pop(prevNum);
+                            numCount -= 2;
+
+                            numStack.push(String(calc));
+                            numCount += 1;
+
+                            operStack.pop(topOper);
+                        }
+                        break;
+                    case '+':
+                        if (!topNum.includes('d') && !prevNum.includes('d')) {
+                            const calc = Number.parseInt(prevNum) + Number.parseInt(topNum);
+
+                            numStack.pop(topNum);
+                            numStack.pop(prevNum);
+                            numCount -= 2;
+
+                            numStack.push(String(calc));
+                            numCount += 1;
+
+                            operStack.pop(topOper);
+                        }
+                        break;
+                    case '-':
+                        if (!topNum.includes('d') && !prevNum.includes('d')) {
+                            const calc = Number.parseInt(prevNum) - Number.parseInt(topNum);
+
+                            numStack.pop(topNum);
+                            numStack.pop(prevNum);
+                            numCount -= 2;
+
+                            numStack.push(String(calc));
+                            numCount += 1;
+
+                            operStack.pop(topOper);
+                        }
+                        break;
+                }
+            }
+        }
+
+        console.log('END',
+            i , 'Num st=>', numStack, 'Oper st=>', operStack, 
+            'Top num', numStack[numStack.length - 1], 
+            'Top op', operStack[operStack.length - 1], 'Cur tok:', tokensArr[i] ); //! END 000000000000000000000000000000000000000000
     }
 
     //TODO: попробовать тут (после выхода из цикла "for") работу со стеком операторов если их остается > 1
+
+    switch(operStack[0]) {
+        case '*':
+            if (!numStack[1].includes('d') && !numStack[0].includes('d')) {
+                result = Number.parseInt(numStack[0]) * Number.parseInt(numStack[1]);
+
+                numStack.pop(numStack[1]);
+                numStack.pop(numStack[0]);
+                numCount -= 2;
+
+                operStack.pop(operStack[0]);
+            }
+            break;
+        case '+':
+            if (!numStack[1].includes('d') && !numStack[0].includes('d')) {
+                result = Number.parseInt(numStack[0]) + Number.parseInt(numStack[1]);
+
+                numStack.pop(numStack[1]);
+                numStack.pop(numStack[0]);
+                numCount -= 2;
+
+                operStack.pop(operStack[0]);
+            }
+            break;
+        case '-':
+            if (!numStack[1].includes('d') && !numStack[0].includes('d')) {
+                result = Number.parseInt(numStack[0]) - Number.parseInt(numStack[1]);
+
+                numStack.pop(numStack[1]);
+                numStack.pop(numStack[0]);
+                numCount -= 2;
+
+                operStack.pop(operStack[0]);
+            }
+            break;
+    }
 
     console.log('Result =>', result);
 
